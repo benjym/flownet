3f825ca1a585cb609847c540fca0e3ce735e5602
benjym <benjy.marks@sydney.edu.au>
2025-07-17 13:32:15 +0000

    Deploying to gh-pages from @ benjym/flownet@f1afed6a6f714ba21b5b25af1037a9d3db077d5a 🚀

---
+++ data_dam_json5-bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkflownet"] = self["webpackChunkflownet"] || []).push([["data_dam_json5"],{
+
+/***/ "./data/dam.json5":
+/*!************************!*\
+  !*** ./data/dam.json5 ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({gridSize:100,points:[{x:0,y:0.5,BC:{type:'EP',value:10}},{x:0.4,y:0.5,BC:{type:'FL'}},{x:0.4,y:0.6,BC:{type:'FL'}},{x:0.6,y:0.6,BC:{type:'FL'}},{x:0.6,y:0.5,BC:{type:'EP',value:5}},{x:1,y:0.5,BC:{type:'FL'}},{x:1,y:1,BC:{type:'FL'}},{x:0,y:1,BC:{type:'FL'}}],solid:[{x:0.4,y:0},{x:0.6,y:0},{x:0.6,y:0.6},{x:0.4,y:0.6}],contourValues:[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]});\n\n//# sourceURL=webpack://flownet/./data/dam.json5?");
+
+/***/ })
+
+}]);
---
+++ data_test_json5-bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkflownet"] = self["webpackChunkflownet"] || []).push([["data_test_json5"],{
+
+/***/ "./data/test.json5":
+/*!*************************!*\
+  !*** ./data/test.json5 ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({gridSize:50,points:[{x:0,y:0,BC:{type:'EP',value:5}},{x:1,y:0,BC:{type:'FL'}},{x:1,y:1,BC:{type:'EP',value:1}},{x:0,y:1,BC:{type:'FL'}}],contourValues:[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]});\n\n//# sourceURL=webpack://flownet/./data/test.json5?");
+
+/***/ })
+
+}]);
