7160684cb7fcce1952f66daf076a4981e1dcabc7
benjym <benjy.marks@sydney.edu.au>
2025-07-18 12:00:58 +0000

    Deploying to gh-pages from @ benjym/flownet@91b7fa55379ff40d94f8effb4da18832ee13058f 🚀

---
+++ data_drain_json5-bundle.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkflownet"] = self["webpackChunkflownet"] || []).push([["data_drain_json5"],{
+
+/***/ "./data/drain.json5":
+/*!**************************!*\
+  !*** ./data/drain.json5 ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({gridSize:100,points:[{x:0,y:0.5,BC:{type:'EP',value:0.4}},{x:1,y:0.5,BC:{type:'FL'}},{x:1,y:1,BC:{type:'FL'}},{x:0,y:1,BC:{type:'FL'}}],drain:{x:0.5,y:0.75,r:0.05,BC:{type:'EP',value:0}}});\n\n//# sourceURL=webpack://flownet/./data/drain.json5?");
+
+/***/ })
+
+}]);
